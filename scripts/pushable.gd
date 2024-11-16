4187557b97616715cafc9a3d65c58f4b66dcb430
Dtay234 <dn5600@rit.edu>
2024-11-16 12:47:21 -0500

    pushable stuff

--- scripts/pushable.gd
+++ scripts/pushable.gd
@@ -1,26 +1,14 @@
 extends RigidBody2D
 
-var locked = true;
+var locked = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	lock_rotation = true;
+	#add_constant_central_force(-get_gravity() / mass)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	var collisions = get_colliding_bodies()
-	for item in collisions:
-		if item.has_method("move_and_slide"):
-			var player = item as CharacterBody2D
-			apply_central_impulse(player.velocity)
 	
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if not body.size == null:
-		if body.size < 3:
-			locked = true
-		else:
-			locked = false
+	pass
