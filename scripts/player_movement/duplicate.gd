5179ac3c2d2f47970d921cedb7ca47c3fbd6e5a8
jjkool1214 <jaysenstolarz@gmail.com>
2024-11-16 16:20:08 -0500

    WAGYUUUUU

--- scripts/player_movement/duplicate.gd
+++ scripts/player_movement/duplicate.gd
@@ -2,6 +2,8 @@ extends State
 
 @export var fall_state: State
 
+@onready var newDroplet = preload("res://scenes/Droplet.tscn")
+
 func enter() -> void:
 	super()
 	
@@ -11,19 +13,27 @@ func enter() -> void:
 	#print(mouse_pos)
 	#print(Input.is_action_just_released("eject"))
 	#print(Input.is_action_just_released("shoot"))
+	print(parent.player_size)
 	if Input.is_action_just_pressed("eject"):
 		# create large droplet of size 1
 		# speedify droplet
 		# set_velocity(cum)
-		parent.player_size = 1
+		if parent.player_size > 1 :
+			print(parent.player_size)
+			parent.projectile_force = (parent.player_size * 400) - 400
+			set_velocity(parent)
+			var droppy_boy = newDroplet.instantiate()
+			droppy_boy.position = parent.position
+			add_sibling(droppy_boy)
+			parent.total_shrink()
 		
-		set_velocity(parent)
 		
 		
 		
 		
 	elif Input.is_action_just_pressed("shoot"):
 		# create large droplet of size parent.player_size - 1
+		print(parent.player_size)
 		parent.shrink()
 
 	
@@ -40,7 +50,6 @@ func set_velocity(node: Node2D) -> void:
 	print("Mouse pos", mouse_pos)
 	print("Player pos", player.position)
 	var angle = atan2(vector_pos.x, vector_pos.y)
-
 	node.velocity.x = parent.projectile_force * vector_pos.x
 	node.velocity.y = parent.projectile_force * vector_pos.y
 	
--- scripts/player_movement/player.gd
+++ scripts/player_movement/player.gd
@@ -25,16 +25,19 @@ func _process(delta: float) -> void:
 func grow() -> void:
 	if player_size < max_player_size:
 		player_size += 1
-	self.scale.x = player_size - size_scaling
-	self.scale.y = player_size - size_scaling
+		self.scale.x = player_size - size_scaling
+		self.scale.y = player_size - size_scaling
 	
 func shrink() -> void:
-	if player_size > size_scaling:
+	if player_size > 1:
 		player_size -= 1
-	self.scale.x = player_size - size_scaling
-	self.scale.y = player_size - size_scaling
+		self.scale.x = player_size - size_scaling
+		self.scale.y = player_size - size_scaling
 
-
+func total_shrink() -> void:
+	player_size = 1
+	self.scale.x = 1
+	self.scale.y = 1
 
 func _on_layer_2_collision_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Pushables"):
