b4db0cd5072152dc3427672f48da6564b2067125
Owen Luts <oluts47@gmail.com>
2024-11-16 19:25:13 -0500

    flower functionality

--- assets/sprites/flower.gd
+++ assets/sprites/flower.gd
@@ -12,3 +12,22 @@ func _process(delta: float) -> void:
 
 func on_complete() -> void:
 	print("level complete!")
+	var scene = get_tree().current_scene.name
+	
+	get_tree().paused = true
+	$AnimatedSprite2D.play("grow")
+	await get_tree().create_timer(1.0).timeout
+	$CPUParticles2D.emitting = true
+	await get_tree().create_timer(3.0).timeout
+		
+	get_tree().paused = false
+
+	match(scene):
+		'Test':
+			get_tree().change_scene_to_file("res://scenes/Level0.tscn")
+		'Level0':
+			get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+		'Level1':
+			get_tree().change_scene_to_file("res://scenes/Level2.tscn")
+		_:
+			get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
