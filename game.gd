e5928cbdb0674a89de954765530bb28043034b04
jjkool1214 <jaysenstolarz@gmail.com>
2024-11-16 22:18:02 -0500

    FInished level nav

--- game.gd
+++ game.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var currLevel = preload("res://scenes/Level0.tscn")
+@export var currLevel : PackedScene
 func die() -> void:
 	get_tree().paused = true
 	await get_tree().create_timer(1.0).timeout
@@ -13,6 +13,8 @@ func reset() -> void:
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("Reset")):
 		get_tree().change_scene_to_packed(currLevel)
+	elif(Input.is_action_just_pressed("Menu")):
+		get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
 		
 func level_end(scene):
 	
@@ -20,9 +22,13 @@ func level_end(scene):
 	match(scene):
 		'Test':
 			get_tree().change_scene_to_file("res://scenes/Level0.tscn")
+			currLevel = preload("res://scenes/Level0.tscn")
 		'Level0':
 			get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+			currLevel = preload("res://scenes/Level1.tscn")
 		'Level1':
 			get_tree().change_scene_to_file("res://scenes/Level2.tscn")
+			currLevel = preload("res://scenes/Level2.tscn")
 		_:
 			get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+			currLevel = preload("res://scenes/MainMenu.tscn")
