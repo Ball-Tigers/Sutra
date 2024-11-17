2e0137af593858c22dad312e6f7f93eda530a3d5
Owen Luts <oluts47@gmail.com>
2024-11-16 20:26:44 -0500

    crosshair update

--- game.gd
+++ game.gd
@@ -2,8 +2,7 @@ extends Node
 
 func die() -> void:
 	get_tree().paused = true
-	await get_tree().create_timer(3.0).timeout
-#	add particle thing here
+	await get_tree().create_timer(1.0).timeout
 	get_tree().reload_current_scene()
 	get_tree().paused = false
 
